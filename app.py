7ca485636427ea49d7d65ac2aba7c1e4015f62cc
Siddhant Srivastava <ssiddhant1009@gmail.com>
2024-07-03 00:25:16 +0530

    a

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from numpy.lib.type_check import imag
 import pygame, sys
 from pygame.locals import *
 import numpy as np
+import tensorflow as tf
+from tensorflow import keras
 from keras.models import load_model
 import cv2
 from tensorflow.python.keras.backend import constant
@@ -83,3 +85,62 @@ while True:
 
             #     label= str(np.argmax(model.predict(image.reshape(1, 28, 28, 1))))
                 
+
+# import pygame,sys
+# from pygame.locals import *
+# import numpy as np
+# from keras.models import load_model
+# import cv2
+
+# WINDOWSIZEX = 640
+# WINDOWSIZEY = 480
+
+# # Initialize Pygame
+# pygame.init()
+
+# pygame.display.set_mode((WINDOWSIZEX, WINDOWSIZEY))
+
+
+
+
+# while  True:
+
+#     for event in pygame.event.get():
+#         if event.type == QUIT:
+#             pygame.quit()
+#             sys.exit()
+
+        # if event.type == MOUSEMOTION and isDrawing:
+        #     xcord, ycord = event.pos
+        #     pygame.draw.circle(DISPLAYSURF, WHITE, (xcord, ycord), 4, 0)
+
+        #     number_xcord.append(xcord)
+        #     number_ycord.append(ycord)
+
+        # if event.type == MOUSEBUTTONDOWN:
+        #     isDrawing = True
+
+        # if event.type == MOUSEBUTTONUP:
+        #     isDrawing = False
+        #     number_xcord = sorted(number_xcord)
+        #     number_ycord = sorted(number_ycord)
+
+        #     rect_min_x , rect_max_x = max(number_xcord[0] - BOUNDRYINC, 0), min(WINDOWSIZEX, number_xcord[-1] + BOUNDRYINC)
+        #     rect_min_y , rect_max_y = max(number_ycord[0] - BOUNDRYINC, 0), min(number_ycord[-1] + BOUNDRYINC, WINDOWSIZEX)
+
+        #     number_xcord = []
+        #     number_ycord = []
+
+        #     img_arr = np.array(pygame.PixelArray(DISPLAYSURF)[rect_min_x:rect_max_x, rect_min_y:rect_max_y]).T.astype(np.float32)
+
+        #     if IMAGESAVE:
+        #         cv2.imwrite("image.png")
+        #         image_count += 1
+            
+        #     # if PREDICT:
+
+        #     #     image = cv2.resize(img_arr, (28, 28))
+        #     #     image = np.pad(image,(10,10), 'constant', constant_values=0)
+        #     #     image = cv2.resize(image, (28, 28))/255
+
+        #     #     label= str(np.argmax(model.predict(image.reshape(1, 28, 28, 1))))
